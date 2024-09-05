0e0d9f9ac0acc8a4bbcfb2f19c629a36783092c7
Wassim <68751630+Wassim-dev-wq@users.noreply.github.com>
2024-09-05 14:54:56 +0200

    initial project setup

---
+++ src/main/java/com/rest/webservices/restwebservices/F1ReactionTimerApplication.java
@@ -0,0 +1,13 @@
+package com.rest.webservices.restwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1ReactionTimerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(F1ReactionTimerApplication.class, args);
+	}
+
+}
