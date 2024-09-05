a276eeaf820b8eb4cc4f317d12b50368b80a4bba
Wassim <68751630+Wassim-dev-wq@users.noreply.github.com>
2024-09-05 15:07:47 +0200

    feat(models): User model implementation

--- src/main/java/com/rest/webservices/restwebservices/models/User.java
+++ src/main/java/com/rest/webservices/restwebservices/models/User.java
@@ -1,4 +1,54 @@
 package com.rest.webservices.restwebservices.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private boolean role;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isRole() {
+        return role;
+    }
+
+    public void setRole(boolean role) {
+        this.role = role;
+    }
 }
