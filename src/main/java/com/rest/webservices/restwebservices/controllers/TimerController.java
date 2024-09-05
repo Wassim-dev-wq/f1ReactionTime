128a463b6571db49ab4f24eff2c9febd1c46ad6b
Wassim <68751630+Wassim-dev-wq@users.noreply.github.com>
2024-09-05 20:13:37 +0200

    feat: submission and retrieval of reaction times

--- src/main/java/com/rest/webservices/restwebservices/controllers/TimerController.java
+++ src/main/java/com/rest/webservices/restwebservices/controllers/TimerController.java
@@ -1,4 +1,33 @@
 package com.rest.webservices.restwebservices.controllers;
 
+import com.rest.webservices.restwebservices.models.Timer;
+import com.rest.webservices.restwebservices.repositories.TimerRepository;
+import com.rest.webservices.restwebservices.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/timers")
 public class TimerController {
+
+    @Autowired
+    private TimerRepository timerRepository;
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/submit-reaction-time")
+    public ResponseEntity<Timer> submitReactionTime(@RequestBody Timer timer) {
+        Timer newTimer = timerRepository.save(timer);
+        return ResponseEntity.ok(newTimer);
+    }
+
+    @GetMapping("/get-reaction-times/{userId}")
+    public ResponseEntity<List<Timer>> getUserReactionTimes(@PathVariable Long userId) {
+        List<Timer> reactionTimes = timerRepository.findByUserId(userId);
+        return ResponseEntity.ok(reactionTimes);
+    }
 }
--- src/main/java/com/rest/webservices/restwebservices/repositories/TimerRepository.java
+++ src/main/java/com/rest/webservices/restwebservices/repositories/TimerRepository.java
@@ -1,4 +1,10 @@
 package com.rest.webservices.restwebservices.repositories;
 
-public class TimerRepository {
+import com.rest.webservices.restwebservices.models.Timer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TimerRepository extends JpaRepository<Timer, Long> {
+    List<Timer> findByUserId(Long userId);
 }
